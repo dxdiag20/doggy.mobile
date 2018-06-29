a9334682cf972d2d099c24e3ace90521c62e15bd
Sejin Jeon <jinaidy93@gmail.com>
2018-06-29 13:28:39 +0900

    Create basic form of AdjustPicActivity

--- Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
@@ -1,4 +1,24 @@
 package com.doggy.doggy;
 
-public class AdjustPicActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class AdjustPicActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_snd_rcv);
+
+        Button adjustButton = findViewById(R.id.adjustButton);
+        adjustButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        startActivity(new Intent(AdjustPicActivity.this, ShowResultActivity.class));
+                    }
+                }
+        );
+    }
 }
