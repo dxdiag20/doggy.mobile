57b1bfd287c6919ef2455954b6d3baa1b2e6a05f
Sejin Jeon <jinaidy93@gmail.com>
2018-06-29 15:15:47 +0900

    Add showing picture feature to AdjustPicActivity
    And fixed a bug (layout mismatch).

--- Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
@@ -1,22 +1,28 @@
 package com.doggy.doggy;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class AdjustPicActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_http_snd_rcv);
+        setContentView(R.layout.activity_adjust_pic);
+
+        // TODO SEJIN: Get image from TakePicActivity and put it in facePictureView.
+        ImageView facePictureView = findViewById(R.id.facePicture);
+        facePictureView.setImageResource(R.drawable.test);
 
         Button adjustButton = findViewById(R.id.adjustButton);
         adjustButton.setOnClickListener(
                 new Button.OnClickListener() {
                     public void onClick(View v) {
-                        startActivity(new Intent(AdjustPicActivity.this, ShowResultActivity.class));
+                        startActivity(new Intent(AdjustPicActivity.this, HttpSndRcvActivity.class));
                     }
                 }
         );
