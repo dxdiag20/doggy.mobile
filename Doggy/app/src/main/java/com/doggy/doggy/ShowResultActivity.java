455f06dac604a763bed5d2f166b0c53a59e6c982
Sejin Jeon <jinaidy93@gmail.com>
2018-06-30 07:55:16 +0900

    Bug fix: Fix stat slide

--- Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
@@ -6,25 +6,29 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 import com.bumptech.glide.Glide;
 
 public class ShowResultActivity extends AppCompatActivity {
-    private ViewPager mViewpager;
+    private RelativeLayout mSlider;
+    private ImageView mImageView2;
     private ImageView mImageView3;
+    private boolean mIsOpen = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_show_result);
-        mViewpager = (ViewPager) findViewById(R.id.viewpager);
-//        mImageView3 = (ImageView) findViewById(R.id.match3);
+        mSlider = findViewById(R.id.slider);
+        mImageView2 = findViewById(R.id.match2);
         mImageView3 = new ImageView(this);
 
 //        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
@@ -36,37 +40,29 @@ public class ShowResultActivity extends AppCompatActivity {
         String serverURL2 = "http://222.112.134.57:5008/static/" + dogName + "/match_2.png";
         String serverURL3 = "http://222.112.134.57:5008/static/" + dogName + "/match_3.png";
         ImageView imageView1 = (ImageView) findViewById(R.id.match1);
-        ImageView imageView2 = (ImageView) findViewById(R.id.match2);
+
 
         Glide.with(this)
                 .load(serverURL1) // image url
                 .into(imageView1);
         Glide.with(this)
                 .load(serverURL2) // image url
-                .into(imageView2);
+                .into(mImageView2);
         Glide.with(this)
                 .load(serverURL3) // image url
                 .into(mImageView3);
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                onBackPressed();
-                return true;
-        }
-
-        ImageButton button = (ImageButton) findViewById(R.id.match2);
-
-        button.setOnClickListener(new Button.OnClickListener() {
+        mImageView2.setOnClickListener(new Button.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mViewpager.addView(mImageView3);
+                if(!mIsOpen) {
+                    mSlider.addView(mImageView3);
+                } else {
+                    mSlider.removeAllViews();
+                }
+                mIsOpen = !mIsOpen;
             }
         });
-
-        return (super.onOptionsItemSelected(item));
     }
 
 }
