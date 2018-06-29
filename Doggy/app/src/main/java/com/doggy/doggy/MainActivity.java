28da7fd7a8c70114fa8aecb02a4f5bb3d5129a1d
Sejin Jeon <jinaidy93@gmail.com>
2018-06-30 01:53:16 +0900

    Change the starting activity from Main to Splash

--- Doggy/app/src/main/java/com/doggy/doggy/MainActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/MainActivity.java
@@ -12,8 +12,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        startActivity(new Intent(this,SplashActivity.class));
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
--- Doggy/app/src/main/java/com/doggy/doggy/SplashActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/SplashActivity.java
@@ -8,40 +8,28 @@ import android.os.Handler;
 import android.util.Log;
 import android.widget.ImageView;
 
-public class SplashActivity extends  Activity {
-
+public class SplashActivity extends Activity {
     private AnimationDrawable animi;
-    private ImageView im ;
-
+    private ImageView im;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_ch);
 
         im = (ImageView) findViewById(R.id.imageAnimation);
         im.setBackgroundResource(R.drawable.animation);
         animi = (AnimationDrawable) im.getBackground();
-
-                animi.start();
+        animi.start();
 
         Log.e("bsos1202", "SplashActivity income!!");
         Handler hd = new Handler();
         hd.postDelayed(new Runnable() {
             @Override
             public void run() {
-
                 finish();
-
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
             }
-
-        }, 2000); // 3초 후 이미지를 닫습니다
-
-
-
-
-
-        }
-
+        }, 4900); // 3초 후 이미지를 닫습니다
+    }
 }
