42c709f63f3983a3fb62862865f35a74cc8d58ab
hangeulBae <dxdiag20@gmail.com>
2018-06-30 05:22:41 +0900

    last

--- Doggy/app/src/main/java/com/doggy/doggy/HttpSndRcvActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/HttpSndRcvActivity.java
@@ -1,17 +1,23 @@
 package com.doggy.doggy;
 
-import android.net.Uri;
-import android.os.Bundle;
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -25,28 +31,28 @@ import okhttp3.Response;
 import org.json.JSONObject;
 
 public class HttpSndRcvActivity extends AppCompatActivity {
-    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/jpg");
-
+    private static final MediaType MEDIA_TYPE_PNG = MediaType.parse("image/jpeg");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         setContentView(R.layout.activity_http_snd_rcv);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
-
         final TextView httpTextViewResult = findViewById(R.id.textView);
-        Intent intent = getIntent();
+        final Intent intentResult = new Intent(getApplicationContext(), ShowResultActivity.class);
         Uri croppedImageUri = getIntent().getParcelableExtra("croppedImageUri");
         Uri resultUri = Uri.parse(croppedImageUri.toString());
         File imageFile = new File(resultUri.getPath());
         httpTextViewResult.setText("나와 닯은 개를 매칭 중입니다");
-        OkHttpClient client = new OkHttpClient();
+        OkHttpClient client = new OkHttpClient.Builder()
+                .connectTimeout(1000, TimeUnit.SECONDS)
+                .readTimeout(1000, TimeUnit.SECONDS)
+                .writeTimeout(1000, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(false)
+                .build();
         RequestBody requestBody = new MultipartBody.Builder().setType(MultipartBody.FORM)
-//                .addFormDataPart("action", "upload")
-//                .addFormDataPart("format", "jpg")
-                .addFormDataPart("image", "image.jpg", RequestBody.create(MEDIA_TYPE_PNG, imageFile))
+                .addFormDataPart("image", "image.jpeg", RequestBody.create(MEDIA_TYPE_PNG, imageFile))
                 .build();
         Request request = new Request.Builder()
                 .url("http://222.112.134.57:5008")
@@ -58,43 +64,30 @@ public class HttpSndRcvActivity extends AppCompatActivity {
                 httpTextViewResult.setText("애플리케이션을 재시작 해주세요.");
                 e.printStackTrace();
             }
-
             @Override
             public void onResponse(Call call, Response response) throws IOException {
-                if (response.isSuccessful()) {
+                if(response.isSuccessful()) {
 
                     final String myResponse = response.body().string();
-                    JSONObject obj = null;
-                    try {
-                        obj = new JSONObject(myResponse);
-                    } catch (Exception e) {
-
-                    }
-
-                    try {
-                        System.out.print(obj.getString("breed"));
-                        System.out.print(obj.getString("description"));
-                        System.out.print(obj.getString("wiki_url"));
-                    } catch (Exception e) {
-
-                    }
                     System.out.print(myResponse);
                     HttpSndRcvActivity.this.runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
                             System.out.println("매칭 완료!");
-                            //httpTextViewResult.setText("매칭 완료!");
+                                httpTextViewResult.setText("매칭 완료");
                         }
                     });
+                    intentResult.putExtra("resultJson",myResponse);
                 }
-                startActivity(new Intent(HttpSndRcvActivity.this, ShowResultActivity.class));
+                startActivity(intentResult);
             }
         });
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        switch (item.getItemId())
+        {
             case android.R.id.home:
                 onBackPressed();
                 return true;
--- Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
@@ -1,12 +1,26 @@
 package com.doggy.doggy;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class ShowResultActivity extends AppCompatActivity {
     @Override
@@ -16,6 +30,23 @@ public class ShowResultActivity extends AppCompatActivity {
         setContentView(R.layout.activity_show_result);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#000000")));
+        Intent intentResult = getIntent();
+        String dogName = intentResult.getStringExtra("resultJson");
+        String serverURL1 = "http://222.112.134.57:5008/static/"+dogName+"/match_1.png";
+        String serverURL2 = "http://222.112.134.57:5008/static/"+dogName+"/match_2.png";
+        String serverURL3 = "http://222.112.134.57:5008/static/"+dogName+"/match_3.png";
+        ImageView imageView1 = (ImageView)findViewById(R.id.match1);
+        ImageView imageView2 = (ImageView)findViewById(R.id.match2);
+        ImageView imageView3 = (ImageView)findViewById(R.id.match3);
+        Glide.with(this)
+                .load(serverURL1) // image url
+                .into(imageView1);
+        Glide.with(this)
+                .load(serverURL2) // image url
+                .into(imageView2);
+        Glide.with(this)
+                .load(serverURL3) // image url
+                .into(imageView3);
     }
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
