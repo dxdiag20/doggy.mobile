60d5fcf388e45dc4591ef77297fc20c969264329
Sejin Jeon <jinaidy93@gmail.com>
2018-06-29 14:01:38 +0900

    Create basic form of HttpSndRcvActivity

--- Doggy/app/src/main/java/com/doggy/doggy/HttpSndRcvActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/HttpSndRcvActivity.java
@@ -1,4 +1,14 @@
 package com.doggy.doggy;
 
-public class HttpSndRcvActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HttpSndRcvActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_http_snd_rcv);
+        startActivity(new Intent(HttpSndRcvActivity.this, ShowResultActivity.class));
+    }
 }
