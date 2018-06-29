ce2898bb3ad1f352df3ea7d577ccb7cf94f8bd49
Sejin Jeon <jinaidy93@gmail.com>
2018-06-29 17:51:51 +0900

    Apply Android-Image-Cropper

--- Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/AdjustPicActivity.java
@@ -1,12 +1,15 @@
 package com.doggy.doggy;
 
 import android.content.Intent;
-import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 public class AdjustPicActivity extends AppCompatActivity {
     @Override
@@ -15,8 +18,18 @@ public class AdjustPicActivity extends AppCompatActivity {
         setContentView(R.layout.activity_adjust_pic);
 
         // TODO SEJIN: Get image from TakePicActivity and put it in facePictureView.
-        ImageView facePictureView = findViewById(R.id.facePicture);
-        facePictureView.setImageResource(R.drawable.test);
+//        Uri facePictureUri = Uri.parse("https://raw.githubusercontent.com/42deSix/Images/master/a_cat_hitting_a_dog.jpg");
+        CropImageView cropImageView = findViewById(R.id.cropImageView);
+//        cropImageView.setImageUriAsync(facePictureUri);
+
+        // start picker to get image for cropping and then use the image in cropping activity
+        CropImage.activity()
+                .setGuidelines(CropImageView.Guidelines.ON)
+                .start(this);
+
+        // start cropping activity for pre-acquired image saved on the device
+//        CropImage.activity(facePictureUri)
+//                .start(this);
 
         Button adjustButton = findViewById(R.id.adjustButton);
         adjustButton.setOnClickListener(
@@ -26,5 +39,21 @@ public class AdjustPicActivity extends AppCompatActivity {
                     }
                 }
         );
+
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                Uri resultUri = result.getUri();
+                ImageView facePictureImageView = findViewById(R.id.facePicture);
+                facePictureImageView.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
+        }
     }
 }
