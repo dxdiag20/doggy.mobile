5ec5682211bad9bacf23cb51c5357d7ae5e5fd5d
Sejin Jeon <jinaidy93@gmail.com>
2018-06-30 01:44:17 +0900

    Merge Hoonmo's code to master

--- Doggy/app/src/main/java/com/doggy/doggy/MainActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        startActivity(new Intent(this,SplashActivity.class));
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
---
+++ Doggy/app/src/main/java/com/doggy/doggy/SplashActivity.java
@@ -0,0 +1,47 @@
+package com.doggy.doggy;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.ImageView;
+
+public class SplashActivity extends  Activity {
+
+    private AnimationDrawable animi;
+    private ImageView im ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_ch);
+
+        im = (ImageView) findViewById(R.id.imageAnimation);
+        im.setBackgroundResource(R.drawable.animation);
+        animi = (AnimationDrawable) im.getBackground();
+
+                animi.start();
+
+        Log.e("bsos1202", "SplashActivity income!!");
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                finish();
+
+            }
+
+        }, 2000); // 3초 후 이미지를 닫습니다
+
+
+
+
+
+        }
+
+}
