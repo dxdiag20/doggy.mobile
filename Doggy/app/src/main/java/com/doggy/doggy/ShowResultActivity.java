7505f19f384a417d98a834786e933483f3e49c1f
Sejin Jeon <jinaidy93@gmail.com>
2018-06-29 14:04:23 +0900

    Create basic form of ShowResultActivity

--- Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
+++ Doggy/app/src/main/java/com/doggy/doggy/ShowResultActivity.java
@@ -1,4 +1,12 @@
 package com.doggy.doggy;
 
-public class ShowResultActivity {
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ShowResultActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_result);
+    }
 }
